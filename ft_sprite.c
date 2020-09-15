63b4e1d8a8bc086935a2a71d2b54da1ded2693cf
Baar Adrien <badrien@mi-r6-p2.s19.be>
2020-09-15 12:27:34 +0200

    norm a bit

--- ft_sprite.c
+++ ft_sprite.c
@@ -6,7 +6,7 @@
 /*   By: badrien <badrien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 12:14:14 by badrien           #+#    #+#             */
-/*   Updated: 2020/09/15 12:15:59 by badrien          ###   ########.fr       */
+/*   Updated: 2020/09/15 12:27:11 by badrien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,40 @@
 
 void	size_sprite(t_mlx *mlx, t_sprite *sprite)
 {
-		sprite->h = mlx->screen_height;
-		sprite->w = mlx->screen_width;
-		sprite->spriteX = mlx->player->sprite_x - mlx->player->posX;
-      	sprite->spriteY = mlx->player->sprite_y - mlx->player->posY;
+	sprite->h = mlx->screen_height;
+	sprite->w = mlx->screen_width;
+	sprite->spriteX = mlx->player->sprite_x - mlx->player->posX;
+	sprite->spriteY = mlx->player->sprite_y - mlx->player->posY;
 
-		sprite->invDet = 1.0 / (mlx->player->planeX * mlx->player->dirY - mlx->player->dirX * mlx->player->planeY);
+	sprite->invDet = 1.0 / (mlx->player->planeX * mlx->player->dirY -
+		mlx->player->dirX * mlx->player->planeY);
 
-		sprite->transformX = sprite->invDet * (mlx->player->dirY * sprite->spriteX - mlx->player->dirX * sprite->spriteY);
-		sprite->transformY = sprite->invDet * ((-(mlx->player->planeY)) * sprite->spriteX + mlx->player->planeX * sprite->spriteY);
-		sprite->spriteScreenX = (int)((sprite->w / 2) * (1 + sprite->transformX / sprite->transformY));
+	sprite->transformX = sprite->invDet * (mlx->player->dirY * sprite->spriteX
+		- mlx->player->dirX * sprite->spriteY);
+	sprite->transformY = sprite->invDet * ((-(mlx->player->planeY)) *
+		sprite->spriteX + mlx->player->planeX * sprite->spriteY);
+	sprite->spriteScreenX = (int)((sprite->w / 2) *
+		(1 + sprite->transformX / sprite->transformY));
 
-		sprite->spriteHeight = abs((int)(sprite->h / (sprite->transformY)));
+	sprite->spriteHeight = abs((int)(sprite->h / (sprite->transformY)));
 }
 
 void	draw_sprite(t_mlx *mlx, t_sprite *sprite)
 {
-			sprite->drawStartY = -(sprite->spriteHeight) / 2 + sprite->h / 2;
-		if (sprite->drawStartY < 0) 
-			sprite->drawStartY = 0;
-		sprite->drawEndY = sprite->spriteHeight / 2 + sprite->h / 2;
-		if (sprite->drawEndY >= sprite->h) 
-			sprite->drawEndY = sprite->h - 1;
+	sprite->drawStartY = -(sprite->spriteHeight) / 2 + sprite->h / 2;
+	if (sprite->drawStartY < 0)
+		sprite->drawStartY = 0;
+	sprite->drawEndY = sprite->spriteHeight / 2 + sprite->h / 2;
+	if (sprite->drawEndY >= sprite->h)
+		sprite->drawEndY = sprite->h - 1;
 
-		sprite->spriteWidth = abs((int) (sprite->h / (sprite->transformY)));
-		sprite->drawStartX = -(sprite->spriteWidth) / 2 + sprite->spriteScreenX;
-		if (sprite->drawStartX < 0) 
-			sprite->drawStartX = 0;
-		sprite->drawEndX = sprite->spriteWidth / 2 + sprite->spriteScreenX;
-		if (sprite->drawEndX >= sprite->w) 
-			sprite->drawEndX = sprite->w - 1;
+	sprite->spriteWidth = abs((int)(sprite->h / (sprite->transformY)));
+	sprite->drawStartX = -(sprite->spriteWidth) / 2 + sprite->spriteScreenX;
+	if (sprite->drawStartX < 0) 
+		sprite->drawStartX = 0;
+	sprite->drawEndX = sprite->spriteWidth / 2 + sprite->spriteScreenX;
+	if (sprite->drawEndX >= sprite->w) 
+		sprite->drawEndX = sprite->w - 1;
 }
 void	add_sprite(t_mlx *mlx, double *ZBuffer)
 {
