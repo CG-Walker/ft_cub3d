3eb32b22e5e2178b5951c10500c1481a908f981c
Baar Adrien <badrien@mi-r6-p2.s19.be>
2020-09-15 12:13:49 +0200

    cut add_sprite into draw_sprite and size_sprite

--- ft_cub3d.c
+++ ft_cub3d.c
@@ -6,7 +6,7 @@
 /*   By: badrien <badrien@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 16:46:34 by cgoncalv          #+#    #+#             */
-/*   Updated: 2020/09/15 11:37:13 by badrien          ###   ########.fr       */
+/*   Updated: 2020/09/15 12:08:44 by badrien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -124,12 +124,8 @@ void	floor_and_sky_text(t_mlx *mlx)
 			}
 }
 
-void	add_sprite(t_mlx *mlx, double *ZBuffer)
+void	size_sprite(t_mlx *mlx, t_sprite *sprite)
 {
-
-		t_sprite *sprite;
-		sprite = malloc(sizeof(t_sprite));
-
 		sprite->h = mlx->screen_height;
 		sprite->w = mlx->screen_width;
 		sprite->spriteX = mlx->player->sprite_x - mlx->player->posX;
@@ -142,8 +138,11 @@ void	add_sprite(t_mlx *mlx, double *ZBuffer)
 		sprite->spriteScreenX = (int)((sprite->w / 2) * (1 + sprite->transformX / sprite->transformY));
 
 		sprite->spriteHeight = abs((int)(sprite->h / (sprite->transformY)));
+}
 
-		sprite->drawStartY = -(sprite->spriteHeight) / 2 + sprite->h / 2;
+void	draw_sprite(t_mlx *mlx, t_sprite *sprite)
+{
+			sprite->drawStartY = -(sprite->spriteHeight) / 2 + sprite->h / 2;
 		if (sprite->drawStartY < 0) 
 			sprite->drawStartY = 0;
 		sprite->drawEndY = sprite->spriteHeight / 2 + sprite->h / 2;
@@ -157,20 +156,29 @@ void	add_sprite(t_mlx *mlx, double *ZBuffer)
 		sprite->drawEndX = sprite->spriteWidth / 2 + sprite->spriteScreenX;
 		if (sprite->drawEndX >= sprite->w) 
 			sprite->drawEndX = sprite->w - 1;
+}
+void	add_sprite(t_mlx *mlx, double *ZBuffer)
+{
 
-		for(sprite->stripe = sprite->drawStartX; sprite->stripe < sprite->drawEndX; sprite->stripe++)
-      	{
-        	sprite->texX = (int)(256 * (sprite->stripe - (-sprite->spriteWidth / 2 + sprite->spriteScreenX)) * texWidth / sprite->spriteWidth) / 256;
-        	if(sprite->transformY > 0 && sprite->stripe > 0 && sprite->stripe < sprite->w && sprite->transformY < ZBuffer[sprite->stripe])
-        	for(sprite->y = sprite->drawStartY; sprite->y < sprite->drawEndY; sprite->y++)
-        	{
-          		sprite->d = (sprite->y) * 256 - sprite->h * 128 + sprite->spriteHeight * 128;
-          		sprite->texY = ((sprite->d * texHeight) / sprite->spriteHeight) / 256;
-          		sprite->color = mlx->texture->sprite[(texWidth * sprite->texY) + sprite->texX];
-          		if((sprite->color & 0x00FFFFFF) != 0) 
-		  			mlx->data[sprite->stripe + (sprite->y * mlx->screen_width)] = sprite->color;
-        	}
-      	}
+	t_sprite *sprite;
+	sprite = malloc(sizeof(t_sprite));
+
+	size_sprite(mlx, sprite);
+	draw_sprite(mlx, sprite);
+
+	for(sprite->stripe = sprite->drawStartX; sprite->stripe < sprite->drawEndX; sprite->stripe++)
+    {
+        sprite->texX = (int)(256 * (sprite->stripe - (-sprite->spriteWidth / 2 + sprite->spriteScreenX)) * texWidth / sprite->spriteWidth) / 256;
+        if(sprite->transformY > 0 && sprite->stripe > 0 && sprite->stripe < sprite->w && sprite->transformY < ZBuffer[sprite->stripe])
+        for(sprite->y = sprite->drawStartY; sprite->y < sprite->drawEndY; sprite->y++)
+        {
+         	sprite->d = (sprite->y) * 256 - sprite->h * 128 + sprite->spriteHeight * 128;
+          	sprite->texY = ((sprite->d * texHeight) / sprite->spriteHeight) / 256;
+          	sprite->color = mlx->texture->sprite[(texWidth * sprite->texY) + sprite->texX];
+          	if((sprite->color & 0x00FFFFFF) != 0) 
+		  		mlx->data[sprite->stripe + (sprite->y * mlx->screen_width)] = sprite->color;
+        }
+    }
 }
 
 void	add_map(t_mlx *mlx)
