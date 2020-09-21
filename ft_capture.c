0937f424723b393adb140cfa504e78dfdbae111f
CG-Walker <cogo97@hotmail.com>
2020-09-21 16:54:45 +0200

    fucking hell.

--- ft_capture.c
+++ ft_capture.c
@@ -5,118 +5,136 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cgoncalv <cgoncalv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/21 12:12:03 by cgoncalv          #+#    #+#             */
-/*   Updated: 2020/09/21 13:21:23 by cgoncalv         ###   ########.fr       */
+/*   Created: 2020/09/21 16:06:34 by cgoncalv          #+#    #+#             */
+/*   Updated: 2020/09/21 16:51:50 by cgoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cub3d.h"
 
 #define CAPTURE_FILENAME "capture.bmp"
-#define BYTES_PER_PIXEL 3
+#define IMG_DEPTH 3
 #define FILE_HEADER_SIZE 14
 #define INFO_HEADER_SIZE 40
-
 #define t_byte unsigned char
 
-void generateBitmapImage(unsigned char* image, t_mlx *mlx);
-unsigned char* createBitmapFileHeader(t_mlx *mlx, int stride);
-unsigned char* createBitmapInfoHeader(t_mlx *mlx);
+int     bmp_write(t_mlx *image, t_byte file_header[FILE_HEADER_SIZE], t_byte info_header[INFO_HEADER_SIZE]);
+void	bmp_write_pixels(int fd, t_mlx *image, t_byte *bmp_data);
+void	bmp_fill_header(t_mlx *image, t_byte file_header[FILE_HEADER_SIZE], t_byte info_header[INFO_HEADER_SIZE]);
 
-int capture(t_mlx *mlx)
+int		capture(t_mlx *state)
 {
-	unsigned char image[mlx->screen_height][mlx->screen_width][BYTES_PER_PIXEL];
+	t_byte	file_header[FILE_HEADER_SIZE];
+	t_byte	info_header[INFO_HEADER_SIZE];
 
-	int i;
-	int j;
-	for (i = 0; i < mlx->screen_height; i++) 
+
+	bmp_fill_header(state, file_header, info_header);
+    printf("SCREEN_WIDTH : %d\n", state->screen_width);
+	if (!bmp_write(state, file_header, info_header))
 	{
-		for (j = 0; j < mlx->screen_width; j++)
-		{
-			image[i][j][2] = (unsigned char) ( i * 255 / mlx->screen_height);             ///red
-			image[i][j][1] = (unsigned char) ( j * 255 / mlx->screen_width);              ///green
-			image[i][j][0] = (unsigned char) ( (i+j) * 255 / (mlx->screen_height + mlx->screen_width)); ///blue
-		}
+		return (1);
 	}
-	generateBitmapImage((unsigned char*)image, mlx);
-	printf("Image generated!!");
+	return (0);
 }
 
-void generateBitmapImage (unsigned char* image, t_mlx *mlx)
+/*
+** bmp file format:
+**     header:
+**         file_header:
+**             2: signature = "BM"
+**             4: file size
+**             4: reserved
+**             4: offset to pixel array
+**         info_header:
+**	       4: header size
+**	       4: image screen_width
+**	       4: image screen_height
+**	       2: number of color planes
+**	       2: bits per pixel
+**	       4: compression
+**	       4: image size
+**	       4: horizontal resolution
+**	       4: vertical resolution
+**	       4: colors in color table
+**	       4: important color count
+**     data:
+**         pixel in rgb format (without alpha component)
+**         padding added at the end of each pixel row
+**         so the length of the row is a multiple of 4
+*/
+
+int     bmp_write(t_mlx *image, t_byte file_header[FILE_HEADER_SIZE],
+						t_byte info_header[INFO_HEADER_SIZE])
 {
-    int widthInBytes = mlx->screen_width * BYTES_PER_PIXEL;
+	int		fd;
+	t_byte	*bmp_data;
 
-    unsigned char padding[3] = {0, 0, 0};
-    int paddingSize = (4 - (widthInBytes) % 4) % 4;
-
-    int stride = (widthInBytes) + paddingSize;
-
-    FILE* imageFile = fopen(CAPTURE_FILENAME, "wb");
-
-    unsigned char* fileHeader = createBitmapFileHeader(mlx, stride);
-    fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
-
-    unsigned char* infoHeader = createBitmapInfoHeader(mlx);
-    fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
-
-    int i;
-    for (i = 0; i < mlx->screen_height; i++) {
-        fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, mlx->screen_width, imageFile);
-        fwrite(padding, 1, paddingSize, imageFile);
-    }
-
-    fclose(imageFile);
+	if ((fd = open(CAPTURE_FILENAME, O_WRONLY | O_CREAT, 0644)) < 0)
+		return (FALSE);
+    printf("SCREEN_WIDTH : %d\n", image->screen_width);
+	if ((bmp_data = malloc(sizeof(unsigned char) *
+			(image->screen_width * IMG_DEPTH))) == NULL)
+	{
+        printf("MALLOC FOIRE\n");
+		close(fd);
+		return (FALSE);
+	}
+	write(fd, file_header, FILE_HEADER_SIZE);
+	write(fd, info_header, INFO_HEADER_SIZE);
+	bmp_write_pixels(fd, image, bmp_data);
+	close(fd);
+	return (TRUE);
 }
 
-unsigned char* createBitmapFileHeader (t_mlx *mlx, int stride)
+void	bmp_write_pixels(int fd, t_mlx *image, t_byte *bmp_data)
 {
-    int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * mlx->screen_height);
+	int		i;
+	int		j;
+	t_byte	padding[3];
+	int		padding_size;
 
-    static unsigned char fileHeader[] = {
-        0,0,     /// signature
-        0,0,0,0, /// image file size in bytes
-        0,0,0,0, /// reserved
-        0,0,0,0, /// start of pixel array
-    };
-
-    fileHeader[ 0] = (unsigned char)('B');
-    fileHeader[ 1] = (unsigned char)('M');
-    fileHeader[ 2] = (unsigned char)(fileSize      );
-    fileHeader[ 3] = (unsigned char)(fileSize >>  8);
-    fileHeader[ 4] = (unsigned char)(fileSize >> 16);
-    fileHeader[ 5] = (unsigned char)(fileSize >> 24);
-    fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
-
-    return fileHeader;
+	ft_bzero(padding, 3);
+	padding_size = (4 - (image->screen_width * IMG_DEPTH) % 4) % 4;
+	i = image->screen_height;
+	while (--i >= 0)
+	{
+		j = -1;
+		while (++j < image->screen_width)
+		{
+			bmp_data[3 * j + 0] = (image->data[4 * (i * image->screen_width + j) + 0]);
+			bmp_data[3 * j + 1] = (image->data[4 * (i * image->screen_width + j) + 1]);
+			bmp_data[3 * j + 2] = (image->data[4 * (i * image->screen_width + j) + 2]);
+		}
+		write(fd, bmp_data, image->screen_width * 3);
+		write(fd, padding, padding_size);
+	}
 }
 
-unsigned char* createBitmapInfoHeader (t_mlx *mlx)
+void	bmp_fill_header(t_mlx *image, t_byte file_header[FILE_HEADER_SIZE],
+						t_byte info_header[INFO_HEADER_SIZE])
 {
-    static unsigned char infoHeader[] = {
-        0,0,0,0, /// header size
-        0,0,0,0, /// image width
-        0,0,0,0, /// image height
-        0,0,     /// number of color planes
-        0,0,     /// bits per pixel
-        0,0,0,0, /// compression
-        0,0,0,0, /// image size
-        0,0,0,0, /// horizontal resolution
-        0,0,0,0, /// vertical resolution
-        0,0,0,0, /// colors in color table
-        0,0,0,0, /// important color count
-    };
+	int	file_size;
 
-	infoHeader[0] = (unsigned char)(INFO_HEADER_SIZE);
-	infoHeader[4] = (unsigned char)(mlx->screen_width);
-	infoHeader[5] = (unsigned char)(mlx->screen_width >> 8);
-	infoHeader[6] = (unsigned char)(mlx->screen_width >> 16);
-	infoHeader[7] = (unsigned char)(mlx->screen_width >> 24);
-	infoHeader[8] = (unsigned char)(mlx->screen_height);
-	infoHeader[9] = (unsigned char)(mlx->screen_height >> 8);
-	infoHeader[10] = (unsigned char)(mlx->screen_height >> 16);
-	infoHeader[11] = (unsigned char)(mlx->screen_height >> 24);
-	infoHeader[12] = (unsigned char)(1);
-	infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL * 8);
-
-	return (infoHeader);
+	file_size = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (IMG_DEPTH * image->screen_width
+			+ ((4 - (image->screen_width * IMG_DEPTH) % 4) % 4)) * image->screen_height;
+	ft_bzero(file_header, FILE_HEADER_SIZE);
+	ft_bzero(info_header, INFO_HEADER_SIZE);
+	file_header[0] = (unsigned char)('B');
+	file_header[1] = (unsigned char)('M');
+	file_header[2] = (unsigned char)(file_size);
+	file_header[3] = (unsigned char)(file_size >> 8);
+	file_header[4] = (unsigned char)(file_size >> 16);
+	file_header[5] = (unsigned char)(file_size >> 24);
+	file_header[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
+	info_header[0] = (unsigned char)(INFO_HEADER_SIZE);
+	info_header[4] = (unsigned char)(image->screen_width);
+	info_header[5] = (unsigned char)(image->screen_width >> 8);
+	info_header[6] = (unsigned char)(image->screen_width >> 16);
+	info_header[7] = (unsigned char)(image->screen_width >> 24);
+	info_header[8] = (unsigned char)(image->screen_height);
+	info_header[9] = (unsigned char)(image->screen_height >> 8);
+	info_header[10] = (unsigned char)(image->screen_height >> 16);
+	info_header[11] = (unsigned char)(image->screen_height >> 24);
+	info_header[12] = (unsigned char)(1);
+	info_header[14] = (unsigned char)(IMG_DEPTH * 8);
 }
